2f06e18cfdc04e1eb55d6411224ca43e2052c9a0
BananaBobBert <45497897+BananaBobBert@users.noreply.github.com>
2022-10-02 18:33:28 +0200

    added penguin room

---
+++ Ludum-Dare-51/Scripts/Enemies/Penguin.cs
@@ -0,0 +1,43 @@
+using Godot;
+
+public class Penguin : AbstractEnemy {
+    [Export]
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Timer moveTimer;
+
+    public override void _Ready() {
+        base._Ready();
+        this.moveTimer = GetNode<Timer>("MoveTimer"); 
+    }
+
+    public override void _IntegrateForces(Physics2DDirectBodyState bodyState) {
+        if (IsHit()) return;
+
+        if (!this.isMoving) {
+            bodyState.LinearVelocity = Vector2.Zero;
+
+            if (Metronome.instance.IsBeat(-1, 0f)) {
+                this.InitMove(bodyState);
+            }
+        }
+    }
+
+    private void InitMove(Physics2DDirectBodyState bodyState) {
+        this.isMoving = true;
+        StartMoveAnimation();
+        this.moveTimer.Start();
+        Vector2 moveDir = (player.GlobalPosition - this.GlobalPosition).Normalized();
+        if (Mathf.Abs(moveDir.x) >= Mathf.Abs(moveDir.y)) {
+            bodyState.LinearVelocity = new Vector2(moveDir.x, 0) * this.moveSpeed;
+        } else {
+            bodyState.LinearVelocity = new Vector2(0, moveDir.y) * this.moveSpeed;
+        }
+        HandleSpriteFlip(moveDir);
+    }
+
+    public void StopMove() {
+        this.isMoving = false;
+    }
+}
