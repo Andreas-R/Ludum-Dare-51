51579e64c716eebe51b64eb8a907aac8747fd834
Andreas Roschal <andreas.roschal.jr@chello.at>
2022-10-03 12:59:11 +0200

    ui

--- Ludum-Dare-51/Scripts/UI/Timeline/Timeline.cs
+++ Ludum-Dare-51/Scripts/UI/Timeline/Timeline.cs
@@ -5,8 +5,9 @@ public class Timeline : Control {
     private static PackedScene largeTickPrefab = ResourceLoader.Load("res://Prefabs/UI/LargeTick.tscn") as PackedScene;
     private static PackedScene smallTickPrefab = ResourceLoader.Load("res://Prefabs/UI/SmallTick.tscn") as PackedScene;
     private static PackedScene imageTickPrefab = ResourceLoader.Load("res://Prefabs/UI/ImageTick.tscn") as PackedScene;
+    private static PackedScene splitTickPrefab = ResourceLoader.Load("res://Prefabs/UI/SplitTick.tscn") as PackedScene;
 
-    public static float timelineLength = 1430f;
+    public static float timelineLength = 1470f;
     public static int numberOfBeatsOnTimeline = 8;
 
     [Export]
@@ -23,9 +24,12 @@ public class Timeline : Control {
     }
 
     public override void _Process(float delta) {
-        if (Metronome.instance.IsBeat(-1, 0)) {
+        if (Metronome.instance.IsBeat(0, 0, -Metronome.instance.BeatToTime(numberOfBeatsOnTimeline * 0.5f))) {
+            this.SpawnSplitTick();
+        } else if (Metronome.instance.IsBeat(-1, 0)) {
             this.SpawnTick(Timeline.largeTickPrefab);
         }
+
         if (Metronome.instance.IsBeat(-1, 0.5f)) {
             this.SpawnTick(Timeline.smallTickPrefab);
         }
@@ -58,6 +62,11 @@ public class Timeline : Control {
     private void SpawnImageTick(Texture tickImage) {
         TextureRect tick = imageTickPrefab.Instance() as TextureRect;
         tick.Texture = tickImage;
+        this.AddChild(tick);
+    }
+
+    private void SpawnSplitTick() {
+        TextureRect tick = splitTickPrefab.Instance() as TextureRect;
         this.AddChild(tick);
     }
 }
